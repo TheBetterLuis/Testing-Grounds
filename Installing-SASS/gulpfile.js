06b89e2467b465dadc1860d04db24c9310dceaf0
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2024-04-13 16:18:26 -0400

    library is working

--- Installing-SASS/gulpfile.js
+++ Installing-SASS/gulpfile.js
@@ -1,14 +1,17 @@
-const { src, dest, watch, series } = require('gulp')
+const { src, dest, watch, series } = require('gulp');
+const { default: gulpPurgeCSS } = require('gulp-purgecss');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
-  return src('shinobi/**/*.scss')
+  return src('sass/**/*.scss')
     .pipe(sass())
+    .pipe(purgecss({ content: ['*.html']}))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['shinobi/**/*.scss'], buildStyles)
+  watch(['sass/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
